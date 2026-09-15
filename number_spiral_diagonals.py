e61c628c5d173a624d9f2bce6867fc6d2bab0a55
Jonathan <jonathanwoollettlight@proton.me>
2026-09-15 20:29:04 +0100

    Solve Project Euler problem 28: Number spiral diagonals

---
+++ number_spiral_diagonals.py
@@ -0,0 +1,11 @@
+# 1^2 + 3^2 + 5^2 + ..
+# n = 5
+n = 1001
+total = 1
+for i in range(3,n+1,2):
+    b = i**2
+    c = i-1
+    total += b + (b-1*c) + (b-2*c) + (b-3*c)
+    # print(total)
+
+print(total)
