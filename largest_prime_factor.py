fef50046c6ed79d6552c0d76c52db81742811e1a
Jonathan <jonathanwoollettlight@proton.me>
2026-09-15 20:23:07 +0100

    Solve Project Euler problem 3: Largest prime factor

---
+++ largest_prime_factor.py
@@ -0,0 +1,27 @@
+from tqdm import tqdm
+
+primes = [2]
+factors = []
+# n = 13195
+n = 600_851_475_143
+
+def isPrime(x):
+    for p in primes:
+        if x % p == 0: return False
+    return True
+
+current = 3
+with tqdm() as pbar:
+    while (current < n):
+        if isPrime(current):
+            primes.append(current)
+            if n % current == 0:
+                n /= current
+                factors.append(current)
+                pbar.set_description(f"factors: {factors}")
+        current += 1
+        pbar.total = n
+        pbar.update(1)
+        pbar.refresh()
+factors.append(current)
+print(factors)
