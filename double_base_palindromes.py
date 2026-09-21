c261b68ee84641ce9571947094330ccb57964d02
Jonathan <jonathanwoollettlight@proton.me>
2026-09-21 23:09:53 +0100

    Solve Project Euler problem 36: Double-base palindromes

---
+++ double_base_palindromes.py
@@ -0,0 +1,21 @@
+import math
+
+def isPalindromic(x, base):
+    n = int(math.log(x, base))
+    # print(f"n: {n}")
+    for i in range((n // 2)+1):
+        upper = (x // base**(n-i)) % base
+        lower = (x // base**i) % base
+        # print(f"upper: {upper}, lower: {lower}")
+        if upper != lower: return False
+    return True
+
+
+# print(isPalindromic(585, 10))
+# print(isPalindromic(585, 2))
+
+x = 0
+for i in range(1,1_000_000):
+    if isPalindromic(i, 10) and isPalindromic(i, 2):
+        x += i
+print(x)
