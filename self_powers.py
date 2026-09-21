dd4085836893971b4e726595b69a5513e9a5b958
Jonathan <jonathanwoollettlight@proton.me>
2026-09-21 20:43:07 +0100

    Solve Project Euler problem 48: Self powers

---
+++ self_powers.py
@@ -0,0 +1,21 @@
+
+# 1^1 + 2^2 + 3^3 + 4^4 + .. + 10^10 = 10_405_071_317
+
+
+digits = 10
+kept = 10**digits
+n = 1000
+x = 0
+y = 0
+for i in range(1,n+1):
+    temp = i
+    for j in range(i-1):
+        temp *= i
+        temp %= kept
+        # print(f"\t{temp}")
+    # print(temp)
+    x += temp
+    x %= kept
+    y += i**i
+print(y)
+print(x)
