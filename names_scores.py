a5f4e6813190c186ca781b047cde1045149256b1
Jonathan <jonathanwoollettlight@proton.me>
2026-09-21 22:01:53 +0100

    Solve Project Euler problem 22: Names scores

---
+++ names_scores.py
@@ -0,0 +1,22 @@
+names = []
+with open("0022_names.txt", "r") as file:
+    names = file.read().replace("\"","").split(",")
+
+names.sort()
+total = 0
+for index,name in enumerate(names):
+    value = 0
+    for char in name:
+        # if name == "COLIN":
+        #     print(f"{name} {ord(char)-ord("A")+1}")
+        value += ord(char)-ord("A")+1
+
+    # if name == "COLIN":
+    #     print(f"{name} {value} {index}")
+
+    value *= index + 1
+
+    # if name == "COLIN":
+    #     print(f"{name} {value} {index}")
+    total += value
+print(total)
